6abd4776e8a790dfd94c89df552d43b423717cc8
romanrudniev <romanrudniev007@gmail/com>
2025-02-14 17:11:43 +0200

    Add backend part 2

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from .models import Contact
 # Create your views here.
 def contact_list(request):
     contacts = Contact.object.all()
-    return render(request, 'app/contacts_list.html', {'contacts': contacts})
+    return render(request, 'contact_list.html', {'contacts': contacts})
