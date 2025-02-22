65fc41a2356524650ce55379045387eb8b81f7d3
romanrudniev <romanrudniev007@gmail/com>
2025-02-22 18:06:00 +0200

    Add backend part 4

--- app/views.py
+++ app/views.py
@@ -9,11 +9,11 @@ def contact_list(request):
 
 
 def add_contact(request):
-    if request.meahod == 'POST':
+    if request.method == 'POST':  # Виправлено
         form = ContactsForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('contact_list')
     else:
         form = ContactsForm()
-        return render(request, 'contact_form.html', {'form': form})
+    return render(request, 'contact_form.html', {'form': form})  # Виправлено відступ
--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,7 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.contact_list, name='contact_list')
+    path('', views.contact_list, name='contact_list'),
+    path('add/', views.add_contact, name='add_contact'),
+
 ]
